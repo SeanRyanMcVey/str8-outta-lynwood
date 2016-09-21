1afee60fc939aee4f5e0c4d643b240cf5f255ba7
David Kessler <dkessler@pillartechnology.com>
2016-09-21 15:19:33 -0400

    removed unnecessary time param, WIP: evaluate to see if busy times match our desired time

--- spec/RoomAvailabilitySpec.js
+++ spec/RoomAvailabilitySpec.js
@@ -1,28 +1,33 @@
 describe('Room availability', function() {
 
-  let sampleResponse = {
+  let sampleResponseWithTimes = {
    "calendars": {
     "pillartechnology.com_38373434343233392d353734@resource.calendar.google.com": {
      "busy": [
       {
        "start": "2016-09-21T13:45:00Z",
        "end": "2016-09-21T14:00:00Z"
-      },
-      {
-       "start": "2016-09-21T17:00:00Z",
-       "end": "2016-09-21T18:00:00Z"
-      },
-      {
-       "start": "2016-09-21T19:00:00Z",
-       "end": "2016-09-21T20:30:00Z"
       }
      ]
     }
    }
  };
 
-  it('should be busy at 13:50', function() {
-    expect(isRoomAvailable("2016-09-21T13:50Z", sampleResponse)).toBe(false);
+ let sampleResponseWithoutTimes = {
+  "calendars": {
+   "pillartechnology.com_38373434343233392d353734@resource.calendar.google.com": {
+    "busy": [
+    ]
+   }
+  }
+};
+
+  it('should be busy when the busy list contains a time range', function() {
+    expect(isRoomAvailable(sampleResponseWithTimes)).toBe(false);
+  });
+
+  it('should be available when the busy list is empty', function(){
+    expect(isRoomAvailable(sampleResponseWithoutTimes)).toBe(true);
   });
 
 });
--- src/RoomAvailability.js
+++ src/RoomAvailability.js
@@ -1,3 +1,4 @@
-function isRoomAvailable() {
-  return false;
+function isRoomAvailable(schedule) {
+  var calendarId = 'pillartechnology.com_38373434343233392d353734@resource.calendar.google.com'
+  return schedule.calendars[calendarId].busy.length == 0;
 }
