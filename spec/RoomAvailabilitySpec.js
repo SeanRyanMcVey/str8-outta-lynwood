d7bff0f679207a9099b73630242195d75d10ef29
selichter <slichter@pillartechnology.com>
2016-09-22 13:13:45 -0400

    mocked out tests for ajax requests

--- spec/RoomAvailabilitySpec.js
+++ spec/RoomAvailabilitySpec.js
@@ -22,6 +22,14 @@ describe('Room availability', function() {
   }
 };
 
+let sampleInput = {
+  "timeMin": "2016-09-21T13:45:00Z",
+  "timeMax": "2016-09-21T14:00:00Z",
+  "timeZone": "string",
+  "groupExpansionMax": 1,
+  "calendarExpansionMax": 1
+};
+
   it('should be busy when the busy list contains a time range', function() {
     expect(isRoomAvailable(sampleResponseWithTimes)).toBe(false);
   });
@@ -29,5 +37,30 @@ describe('Room availability', function() {
   it('should be available when the busy list is empty', function(){
     expect(isRoomAvailable(sampleResponseWithoutTimes)).toBe(true);
   });
+
+  beforeEach(function(){
+    jasmine.Ajax.install();
+  });
+
+  afterEach(function(){
+    jasmine.Ajax.uninstall();
+  });
+
+  it('gets freebusy calendar data', function() {
+    getFreebusyCal();
+    expect(jasmine.Ajax.requests.mostRecent().url).toBe('https://www.googleapis.com/calendar/v3/freeBusy');
+    expect(jasmine.Ajax.requests.mostRecent().method).toBe('POST');
+  });
+
+  // it('sends input provided by user', function() {
+  //   var testInput = JSON.stringify(sampleInput);
+  //   getFreebusyCal(testInput);
+  //   console.log(jasmine.Ajax.requests.mostRecent().data());
+  //   expect(jasmine.Ajax.requests.mostRecent().data()).toEqual(testInput);
+  //
+  // });
+
+
+
 
 });
--- src/RoomAvailability.js
+++ src/RoomAvailability.js
@@ -2,3 +2,9 @@ function isRoomAvailable(schedule) {
   var calendarId = 'pillartechnology.com_38373434343233392d353734@resource.calendar.google.com'
   return schedule.calendars[calendarId].busy.length == 0;
 }
+
+function getFreebusyCal(input) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://www.googleapis.com/calendar/v3/freeBusy");
+  xhr.send(input);
+}
